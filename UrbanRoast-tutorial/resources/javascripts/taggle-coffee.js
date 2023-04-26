8d66ce21f4da66ccdc8d43ad3cfac3f45d9c8908
Matthew Barger <mbarger@khinc.com>
2023-04-26 15:56:03 -0500

    done

--- UrbanRoast-tutorial/resources/javascripts/taggle-coffee.js
+++
@@ -1,6 +0,0 @@
-var taggle = new Taggle('example6');
-
-taggle.add(coffeeSelection1);
-taggle.add(['two', 'three', 'four', 'four', 'five', 'five']);
-taggle.remove('five');
-taggle.remove('four', true);
